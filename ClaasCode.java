60ccf33ef26579354ef203b812ba3d53cc78bcbc
emorm2 <96049316+emorm2@users.noreply.github.com>
2021-12-14 18:47:00 +0200

    Update ClaasCode.java

--- ClaasCode.java
+++ ClaasCode.java
@@ -17,5 +17,26 @@ public class Main {
         else {
             System.out.println("not good!");
         }
+        
+        //1.Write program that create 2 boolean variables:
+        //Boolean WashHands;
+        //Boolean OrderMeal;
+        //Give them values of true / false.
+        //And check if both conditions are true in order for the user to be
+        // able to eat. Write an appropriate description.
+        
+        
+         public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("did you wash hands?");
+        boolean washHands = s.nextBoolean();
+        System.out.println("did you oder your meal??");
+        boolean orderMeal = s.nextBoolean();
+
+        if (washHands == true && orderMeal == true ) {
+            System.out.println("You can eat!");
+        }else {
+            System.out.println("you can't eat");
+        }
     }
 }
